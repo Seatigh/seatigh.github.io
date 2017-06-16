2874669e389efb09a54d0d944146a96efa4a9222
Seatigh <100303493@alumnos.uc3m.es>
2017-06-16 18:21:08 +0200

    simple version finished

--- dot2point.py
+++ dot2point.py
@@ -3,8 +3,6 @@ result = []
 with open('dot_world_map.txt') as dot_map:
     for i, line in enumerate(dot_map):
         for j, char in enumerate(line):
-            if char == '*':
-                result.append([(j+1)*16+10, i*16+10])
-
-for i, j in result:
-    print('ctx.ellipse({}, {}, 5, 5, 0, 0, 8);'.format(i, j))
+            if char in ['a', 's', 'e', '*', '-', 'o']:
+                result.append([(j+1)*16+10, i*16+10, char])
+print(result)
