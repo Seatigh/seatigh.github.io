17e3fbc0c2d4d94b0ced7309b764ce0450cfddd5
seatigh <arubiocastellano@gmail.com>
2019-12-17 00:32:45 +0100

    Guayota entry: - Extended update. - Main structure designed. - TODO: EL DESARROLLO

--- js/main.js
+++ js/main.js
@@ -12,9 +12,8 @@ $(document).ready(function(){
 		});
 
 		$("#return-top").click(function () {
-			$('body, html').animate({scrollTop: 0}, 1000);
+			$('body, html').animate({scrollTop: 0}, 0);
 			return false;
 		});
 	});
-
 });
